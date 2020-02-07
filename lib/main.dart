6f72d01190895986eba059672c594ea2954a551a
ricky <rikanthanricky@gmail.com>
2020-02-07 11:55:21 +0530

    buttonpressed

--- lib/main.dart
+++ lib/main.dart
@@ -28,43 +28,63 @@ class Home extends StatelessWidget
   }
 }
 
-class page extends StatelessWidget
-{
+class page extends StatefulWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    var leftdice=Random().nextInt(6);
-    var rightdice=Random().nextInt(6);
-    return Center(
-      child: Row
-        (
-        children: <Widget>[
-          Expanded
-            (
+  _page createState() => _page();
+}
 
+class _page extends State<page> {
+            @override
+            Widget build(BuildContext context)
+            {
+            var leftdice=Random().nextInt(6);
+            var rightdice=Random().nextInt(6);
+            return Center(
+            child: Row
+            (
+            children: <Widget>[
+            Expanded
+            (
 
             child:FlatButton(
+                child: Image.asset('images/dice$leftdice.png'),
 
-              child: Image.asset('images/dice$leftdice.png'),
-              onPressed: (){
-                leftdice=Random().nextInt(6);
-              },
+            onPressed: (){
+              setState(()
+                {
+                  leftdice=Random().nextInt(6);
+                }
+              );
+
+            },
             ),
-          ),
-          Expanded
+            ),
+            Expanded
             (
 
-              child: FlatButton(
-                child: Image.asset('images/dice$rightdice.png'),
-                onPressed: (){
-                  rightdice=Random().nextInt(6);
-                },
+            child: FlatButton(
+            child: Image.asset('images/dice$rightdice.png'),
+            onPressed: (){
 
-            ),
-          ),
-        ]
+              setState(()
+              {
+                rightdice=Random().nextInt(6);
+              }
+              );
 
-      ),
-    );
-  }
+
+            },
+),
+
+),
+
+],
+
+),
+);
 }
+}
+
+
+
+
